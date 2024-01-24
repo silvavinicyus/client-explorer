431b997e4f84d458bc501729b02a32184f286ddd
Vinícyus Silva <vinicyus346@gmail.com>
2024-01-24 16:05:49 -0300

    feat: starting client explorer project

---
+++ backend/src/1-domain/entities/client.ts
@@ -0,0 +1,44 @@
+import { Right, right } from "../../shared/either";
+import { AbstractEntity } from "../abstractEntity";
+import { ITimestamps } from "../timestamps";
+
+export interface IClientEntity extends ITimestamps {
+  id?: number
+  uuid?: string
+  name: string
+  email: string
+  phone: string
+}
+
+export type IInputClientEntity = Pick<IClientEntity, 'email' | 'name' | 'phone'>
+
+export type IClientEntityKeys = Pick<IClientEntity, 'email' | 'name' | 'id' | 'uuid'>
+
+export class ClientEntity extends AbstractEntity<IClientEntity> {
+  static create(
+    props: IInputClientEntity,
+    currentDate: Date
+  ): Right<void, ClientEntity> {
+    const clientEntity = new ClientEntity({
+      id: undefined,
+      uuid: undefined,
+      created_at: currentDate,
+      updated_at: currentDate,
+      ...props
+    })
+
+    return right(clientEntity)
+  }
+
+  static update(
+    props: Partial<IClientEntity>,
+    currentDate: Date
+  ): Right<void, ClientEntity> {
+    const clientEntity = new ClientEntity({
+      ...props,
+      updated_at: currentDate,
+    } as IClientEntity)
+
+    return right(clientEntity)
+  }
+}
