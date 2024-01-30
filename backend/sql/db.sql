e482a49c03e3fbd93158cdf728cbf1d4dbfd737d
Vinicyus Silva <vinicyus346@gmail.com>
2024-01-30 01:38:57 -0300

    fix: adding address to clients table creation

--- backend/sql/db.sql
+++ backend/sql/db.sql
@@ -8,6 +8,7 @@ CREATE TABLE clients (
   name varchar(40) NOT NULL,
   email varchar(40) UNIQUE NOT NULL,
   phone varchar(15) NOT NULL,
+  address varchar(10) NOT NULL,
   created_at timestamp NOT NULL,
   updated_at timestamp NOT NULL 
 )
